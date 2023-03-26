d85593ead004a2ff96455bed9a0794ecc08f6e27
Gustavo Costa Pereira <gustavo.costa@lojainterativa.com>
2023-03-26 16:51:43 -0300

    Primeiro commit com o component nav e sistema de rotas criado

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {ButtonModule} from "primeng/button";
+import {MenubarModule} from "primeng/menubar";
+import {FormsModule} from "@angular/forms";
+import {TieredMenuModule} from "primeng/tieredmenu";
+import {MenuModule} from "primeng/menu";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ButtonModule,
+    MenubarModule,
+    FormsModule,
+    TieredMenuModule,
+    MenuModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/nav/nav.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { OnInit } from "@angular/core";
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent {
+
+  modo = false;
+  expandMenu = false;
+  buttonData: any = [
+    {label: 'Dashboard', icon: 'home', selected: true, id: 0, hidden: true},
+    {label: 'Agente', icon: 'home', id: 1, hidden: true},
+    {label: 'Área', icon: 'home', id: 2, hidden: true},
+    {label: 'Setor', icon: 'home', id: 3, hidden: true},
+    {label: 'Quarteirão', icon: 'home', id: 4, hidden: true},
+    {label: 'Relatórios', icon: 'home', iconExpandMenu: 'chevron-right', iconExpandedMenu: 'chevron-down', id: 5, hidden: true},
+    {label: 'Boletim', icon: 'home', id: 6, hidden: false, exapandMenu: true},
+    {label: 'Feedback', icon: 'home', id: 7, hidden: true},
+  ];
+  expanded(button: any){
+    if (button === 5){
+      this.buttonData[6].hidden = !this.buttonData[6].hidden;
+    }
+  }
+  modoNoturno() {
+    this.modo = !this.modo;
+  }
+
+  constructor() {}
+
+  ngOnInit() {}
+
+}
+
